463ffe72dc954d26c9e9715cc192ea6ea89d8065
tom_glb <goodMorning_glb@guigu.com>
2020-12-04 15:35:24 +0800

    second changes!

--- src/main/java/TestGithub.java
+++ src/main/java/TestGithub.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Copyright © 2016 首都信息发展股份有限公司  All rights reserved
  *
@@ -8,5 +11,17 @@
 public class TestGithub {
     public static void main(String[] args) {
         System.out.println ("modify changes commit ............");
+
+        System.out.println ("-------begin call method------");
+        Map<String, Object> params = new HashMap<String, Object> ();
+        params.put ("xingming", "张三");
+        params.put ("zjlx", "01");
+        params.put ("zjhm", "110228198503083322");
+        test(params);
+        System.out.println ("------------end method -------");
+    }
+
+    public static void test(Map<String, Object> params) {
+        System.out.println (params.get ("xingming"));
     }
 }
